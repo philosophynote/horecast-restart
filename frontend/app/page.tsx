ddd67c9701c444294912d8d1376186d0a3bf3b8c
philosophynote <adverdest@gmail.com>
2023-12-12 19:54:58 +0900

    コメントアウト追加

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 // // import Image from 'next/image'
 import { useState, useEffect } from "react"
-import { fetchTrack } from '@/lib/data';
+// import { fetchTrack } from '@/lib/data';
 import { TableHead, TableRow, TableHeader, TableCell, TableBody, Table } from "@/components/ui/table"
 import { Badge } from "@/components/ui/badge"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
