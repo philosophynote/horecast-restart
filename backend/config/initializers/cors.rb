9dcf713c99daf4fd5325af845b2756402577da98
philosophynote <adverdest@gmail.com>
2023-12-16 18:44:49 +0900

    ENVが無い場合のCORS設定を修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONTEND_HOST"]
+    origins ENV["FRONTEND_HOST"] || ""
 
     resource "*",
       headers: :any,
